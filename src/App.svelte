d0d1f9dd809791f6bc4af56b25c4c1032a7b6e51
Joshua Richardson <joshua@richardson.tech>
2019-08-03 23:49:12 +0100

    Initial svelte router done-ish?

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,31 @@
 <script>
-	export let name;
+    import "../node_modules/materialize-css/dist/css/materialize.min.css";
+    import '../node_modules/materialize-css/dist/js/materialize.min.js';
+    import Navbar from "./components/Navbar.svelte";
+
+    import Home from "./routes/Home.svelte"
+    import About from "./routes/About.svelte"
+
+    import Router from 'svelte-spa-router'
+
+    const routes = {
+        '/': Home,
+        "/about": About,
+    };
+
+
+    export let url = "";
+
+    export let name;
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+    :global(body) {
+        margin: 0;
+        padding: 0;
+    }
 </style>
 
-<h1>Allo {name}!</h1>
+<Navbar></Navbar>
+
+<Router {routes} />
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,36 @@
+<script>
+
+
+</script>
+
+
+<style>
+    #logo-container {
+        margin-left: 10px;
+    }
+
+    a:hover {
+        text-decoration: none;
+    }
+
+    a:visited {
+        color: white;
+    }
+
+</style>
+
+
+<nav class="light-blue lighten-1" role="navigation">
+    <a id="logo-container" href="#!" class="brand-logo">Aster</a>
+    <ul class="right hide-on-med-and-down">
+        <li><a href="#/">Home</a></li>
+        <li><a href="#/about">About</a></li>
+    </ul>
+
+    <ul id="nav-mobile" class="sidenav">
+        <li><a href="#/">Home</a></li>
+        <li><a href="#/about">About</a></li>
+    </ul>
+    <a href="#!" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+
+</nav>
