3a0ecca09c997e8c3fbc215988b8db4fe431f174
Joshua Richardson <joshua@richardson.tech>
2019-08-16 01:32:45 +0100

    Adds beginnings of some solidity

---
+++ solidity/contracts/Crowdfund.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.5.8;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract Aster {
+    using SafeMath for uint256;
+
+}
+
+
+contract Campaign {
+    using SafeMath for uint256;
+    
+    
+}
